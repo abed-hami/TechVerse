491a6cfa7177d771d88a5208352c4bb16307146a
abedhamiyeh <abedhamye28@gmail.com>
2024-08-19 23:15:15 +0300

    front-end home page

--- frontend/TechVerse_frontend/src/app/app.component.ts
+++ frontend/TechVerse_frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./common/navbar/navbar.component";
+import { HomeComponent } from "./common/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
