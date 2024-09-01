db331ae6d142bf9af6f02f1f4f25125897f5add1
abedhamiyeh <abedhamye28@gmail.com>
2024-09-01 15:32:17 +0300

    footer & products sections

--- frontend/TechVerse_frontend/src/app/app.component.ts
+++ frontend/TechVerse_frontend/src/app/app.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./common/navbar/navbar.component";
 import { HomeComponent } from "./common/home/home.component";
+import { CategoriesComponent } from "./common/categories/categories.component";
+import { CommercialComponent } from "./common/commercial/commercial.component";
+import { ProductsComponent } from "./common/products/products.component";
+import { FooterComponent } from "./common/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent, CategoriesComponent, CommercialComponent, ProductsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/TechVerse_frontend/src/app/common/navbar/navbar.component.ts
+++ frontend/TechVerse_frontend/src/app/common/navbar/navbar.component.ts
@@ -9,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  viewNavigation(){
+    let links=document.getElementsByClassName("navlinks");
+    let navicons=document.getElementsByClassName("navicons");
+    let icon = document.getElementsByClassName("bi-list");
+    let ficon = document.getElementsByClassName("bi-x");
+    let view = document.getElementsByClassName("view")
+
+    if(!view[0]){
+      links[0].classList.add("view")
+      navicons[0].classList.add("view")
+      icon[0].classList.add("bi-x")
+      icon[0].classList.remove("bi-list")
+    }
+    else if(view[0]){
+      links[0].classList.remove("view")
+      navicons[0].classList.remove("view")
+      ficon[0].classList.add("bi-list")
+      ficon[0].classList.remove("bi-x")
+    }
+
+  }
+
 }
